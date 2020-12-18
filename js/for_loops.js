d55831c0b14e3e3043d23da3a206156f9ad75af7
Carlos Padilla <mr.carlos.padilla@gmail.com>
2020-12-18 14:22:22 -0600

    adding loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,28 @@
+"use strict"
+
+function showMultiplicationTable(num) {
+    for(var x = 1; x <= 10; x++) {
+        var result = x * num;
+        console.log(num + ' * ' + x + ' = ' + result);
+    }
+}
+showMultiplicationTable(7);
+
+for(var x = 1; x <= 10; x++) {
+    var randomNumber = Math.floor(Math.random() * (200 - 20) + 20);
+    if(randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even.");
+    } else {
+        console.log(randomNumber + " is odd.");
+    }
+}
+
+// function repeatedNumbers(num){
+//     for(var x = 1; x < 10; x++) {
+//
+//     }
+// }
+
+for(x = 100; x >= 5; x -= 5) {
+    console.log(x);
+}
