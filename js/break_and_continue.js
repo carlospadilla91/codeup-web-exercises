f1d46fe60583cb67a1c05e49eb81dd9d646d9ba4
Carlos Padilla <mr.carlos.padilla@gmail.com>
2020-12-20 20:39:28 -0600

    updated break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,13 +1,12 @@
 "use strict"
 
-// var oddNumber = prompt("Enter an odd number between 1-50");
-// console.log("Number to skip is " + oddNumber);
-//
-// for(var i = 1; i <= 50; i++) {
-//     if(oddNumber % 2 === 0){
-//     break;
-//     }
-//     if(oddNumber % 2 === 1) {
-//         console.log("Here is an odd number: " + oddNumber);
-//     }
-// }
+var oddNumber = prompt("Enter an odd number between 1-50");
+console.log("Number to skip is " + oddNumber);
+
+while(oddNumber % 2 === 1) {
+    if(oddNumber % 2 !== 1){
+        break;
+}   if(oddNumber % 2 === 1) {
+        console.log("Here is an odd number: " + oddNumber);
+    }
+}
