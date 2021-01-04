094e35b5f6264abe5136a7964a6e3c61939c39de
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-01-04 15:55:28 -0600

    updated arrays exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -23,8 +23,10 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-    var planetsToString = document.write(planetsArray.join(" <br> "));
+    var planetsToString = (planetsArray.join(" <br> "));
     console.log(planetsToString);
 
+    var planetsToList = planetsArray.join(" <li> ");
+    console.log(" <ul> " + planetsToList + " </ul> ");
 
 })();
