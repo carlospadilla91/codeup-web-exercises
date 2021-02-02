3ff3898940f11998e59930dbd9dff692dc5e96a7
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-02-02 14:41:21 -0600

    completed jquery selectors exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -7,3 +7,22 @@ window.onload = function () {
 $(document).ready(function () {
     alert("Page has loaded");
 });
+
+$(document).ready(function () {
+   var contents = $("#header-1").html();
+   alert(contents);
+   var content = $("#header-2").html();
+   alert(content);
+});
+// can i alert content without having to create a var?
+
+$(document).ready(function () {
+    $(".codeup").css("border", "solid red 1px");
+});
+
+$(document).ready(function () {
+    $("li").css("font-size", "20px" );
+    $("h1, p, li").css("background-color", "gray");
+    var contents = $("h1").text();
+    alert(contents);
+});
