722b0ee3e68338113b3f4dbb148fd13df207e5d6
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-02-10 21:30:24 -0600

    additional dom review

--- js/dom-review.js
+++ js/dom-review.js
@@ -16,21 +16,24 @@
 //hover usually takes in two parameters
 // $('.card').hover(hoverIn, hoverOut);
 
-//toggle class added from html with click
-// $('.card').click(function (){
-//     $(this).toggleClass("toggleClass");
-// });
-//selector is all li that are children of class "list-group"
-$(".list-group>li").dblclick(function (){
-   $(this).text("I GOT DBL CLICKED!");
-   $(this).click(function (){
-       $(this).text("ahhhhh...better");
-   })
+// toggle class added from html with click
+$('.card').click(function (){
+    $(this).toggleClass("toggleClass");
 });
+//selector is all li that are children of class "list-group"
+// $(".list-group>li").dblclick(function (){
+//    $(this).text("I GOT DBL CLICKED!");
+//    $(this).click(function (){
+//        $(this).text("ahhhhh...better");
+//    })
+// });
 
+// $("#first").val($("#first").val() + "carlos")
+var input = $("#first");
+input.val($("#first").val() + "Yasin");
 // GET DATA
 
-// this function is making a new object
+// this function is making a new object from the user input
 function getInputData() {
     let first = $("#first").val();
     let last = $("#last").val();
@@ -40,24 +43,26 @@ function getInputData() {
         id: 4, first , last, handleField
     };
 }
-$("#submitBtn").click(function () {
-    console.log(getInputData());
-
-    let inputData = getInputData()
-
-// create the elements that match the html
-//     let cellCount = $('tr').first().children().length;
+// $("#submitBtn").click(function () {
+//     console.log(getInputData());
 //
-    let tableRow = $("<tr></tr>");
+//     let inputData = getInputData()
+//
+// // create the elements that match the html
 
-    let td1 = $("<td></td>").text(inputData.id);
-    let td2 = $("<td></td>").text(inputData.first);
-    let td3 = $("<td></td>").text(inputData.last);
-    let td4 = $("<td></td>").text(inputData.handleField);
-
-    tableRow.append([td1, td2, td3, td4]);
-    tableRow.appendTo($("tbody"));
-})
+        // this is getting the length of children inside the first <tr>
+//       let cellCount = $('tr').first().children().length;
+// //
+//     let tableRow = $("<tr></tr>");
+//
+//     let td1 = $("<td></td>").text(inputData.id);
+//     let td2 = $("<td></td>").text(inputData.first);
+//     let td3 = $("<td></td>").text(inputData.last);
+//     let td4 = $("<td></td>").text(inputData.handleField);
+//
+//     tableRow.append([td1, td2, td3, td4]);
+//     tableRow.appendTo($("tbody"));
+// })
 //
 //
 //
@@ -77,7 +82,8 @@ $("img").click(function (){
     $(this).attr("alt", "codeup");
 })
 
-// $("#submitBtn").click(function () {
-//     window.location.replace("https://justscream.baby/");
-//     // location.reload();
-// })
+//this function
+$("#submitBtn").click(function () {
+    window.location.replace("https://justscream.baby/");
+    // location.reload();
+})
