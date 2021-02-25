94196e8f4fc4a4caac3f3caa7be7251bbb5f97b2
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-02-25 17:16:32 -0600

    feat: completed promises exercise

--- js/promises.js
+++ js/promises.js
@@ -15,13 +15,46 @@
 //     }, num);
 // }
 
-function wait(num) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        },num)
-    })
+// function wait(num) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve();
+//         },num)
+//     })
+// }
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+//
+
+const githubUrl = 'https://api.github.com/users/'
+
+// fetch(githubUrl)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+
+function recentPush(username) {
+    return new Promise(((resolve,reject) => {
+        resolve(fetch(githubUrl + username,{headers: {'Authorization': GITHUB_TOKEN}})
+            .then(response => response.json())
+            .then(data => data.updated_at))
+        reject(console.error)
+    }))
 }
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+console.log(recentPush("carlospadilla91"));;
+
+// function getDateOfLastCommit(username) {
+//     const endpoint = `/users/${username}/events/public`
+//     const config = {
+//         headers: {
+//             'Authorization': `token ${GITHUB_TOKEN}`
+//         }
+//     }
+//     return fetch(githubUrl  + endpoint, config)
+//         .then(response => response.json())
+//         .then(console.log)
+// }
+//
+// getDateOfLastCommit("carlospadilla91").then(console.log)
