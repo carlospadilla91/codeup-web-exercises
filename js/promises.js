222bfaba21312f4d4a6895da733b16012a737a92
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-02-24 22:20:28 -0600

    feat: complete with first part of promises exercise

--- js/promises.js
+++ js/promises.js
@@ -2,14 +2,26 @@
 
 // Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 
-function wait(num){
-    setTimeout(() => {
+// function wait(num){
+//     setTimeout(() => {
+//     return new Promise((resolve, reject) => {
+//             if (num === 'number') {
+//                 resolve();
+//             }
+//             else {
+//                 reject();
+//             }
+//         })
+//     }, num);
+// }
+
+function wait(num) {
     return new Promise((resolve, reject) => {
-            if (num === 'number') {
-                resolve("Success")
-            }
+        setTimeout(() => {
+            resolve();
+        },num)
     })
-    }, num);
 }
 
-wait(2000).then(console.log("You'll see this after 2 seconds"));
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
